773c57b16dbf71ccd350585989d7c7ec100cb264
feron911 <63519699+feron911@users.noreply.github.com>
2020-09-23 07:30:55 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -2,10 +2,12 @@ def calcul(a, b, c):
     if c == 0 and b == '/':
         return "Zero division error"
     return eval(a+b+c)
+
+
 print('Insert first number and press enter:')
 fn = input()
 print('Insert math symbol(+,-,*,/) and press enter:')
 ms = input()
 print('Insert second number and press enter:')
 sn = input()
-print('Answer is', calcul(fn,ms,sn))
+print('Answer is', calcul(fn, ms, sn))
